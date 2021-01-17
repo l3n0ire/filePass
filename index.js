986f12907255c266a61d619ab69a27d17b9a9d2d
Colin Lin <colincool100@gmail.com>
2021-01-16 20:10:47 -0500

    project files

---
+++ index.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const firebase = require('firebase');
+var admin = require("firebase-admin");
+var urls=[]
+
+
+// body parser
+app.use(express.json());
+// set static folder
+app.use(express.static(path.join(__dirname,'public')))
+// firebase config
+var firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    databaseURL: process.env.databaseURL,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    type:process.env.type,
+    private_key_id: process.env.private_key_id,
+    private_key:process.env.private_key.replace(/\\n/g, '\n'),
+    client_email:process.env.client_email,
+    client_id:process.env.client_id,
+    auth_uri : process.env.auth_uri,
+    token_uri : process.env.token_uri,
+    auth_provider_x509_cert_url : process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url
+  };
+firebase.initializeApp(firebaseConfig);
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig),
+  databaseURL: process.env.databaseURL
+});
+
+var bucket = admin.storage().bucket(process.env.storageBucket);
+
+// download file
+app.get('/api/files/:downloadCode', async function (req, res) {
+  let folderName = req.params.downloadCode+'/'
+  
+  bucket.getFiles({prefix:folderName, delimiter:'/', autoPaginate:false},function(err, files) {
+      if (!err) {
+        // check if folder exists
+        if(files.length!=0)
+        {
+          files.forEach(file=>{
+            urls.push(file.metadata.mediaLink)
+          });
+          res.send({"urls":urls});
+          // clear urls
+          urls =[];
+        }
+        else{
+            res.status(400).json({"message":"folder not found"})
+        }
+        
+      }
+      else
+      {
+        console.log("error");
+        res.status(400).json({"message":err})
+      }
+  });
+})
+
+// get keys
+app.get('/api/keys', function (req, res) {
+    res.send(firebaseConfig)
+})
+
+// upload file
+app.post('/api/files', function (req, res) {
+    if("downloadCode" in req.body && "password" in req.body && "expireTime" in req.body){
+        try{
+            addToDB(req.body.downloadCode,req.body.password,req.body.expireTime);
+            res.send({"message":"added to db"})
+        }catch(error){
+            console.log(error)
+            res.status(500).json("server error")
+        }
+    }
+    else
+        res.status(400).json("missing parameter")
+})
+
+async function addToDB(downloadCode,password,expireTime){
+
+    const res = await firebase.database().ref("users/"+downloadCode).set({
+      downloadCode:downloadCode,
+      password:password,
+      expireTime:expireTime
+    });  
+}
+
+const PORT= process.env.PORT || 5000;
+console.log(process.env.apiKey)
+app.listen(PORT,()=>console.log(`server running ${process.env.NODE_ENV} on port ${PORT}`));
---
+++ public/firebase.js
@@ -0,0 +1,83 @@
+const baseUrl = "https://file2go.herokuapp.com"
+//const baseUrl = "http://localhost:5000"
+// Your web app's Firebase configuration
+fetch(baseUrl+'/api/keys')
+  .then(res=>res.json())
+  .then(firebaseConfig=>{
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    console.log(firebase);
+  });
+
+async function random()
+{
+  let data = {
+    "jsonrpc": "2.0",
+    "method": "generateStrings",
+    "params": {
+        "apiKey": "53b56391-d01d-40b7-984b-1a92afe63182",
+        "n": 1,
+        "length": 6,
+        "characters": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "replacement": true
+    },
+    "id": 42
+  }
+  const response = await fetch('https://api.random.org/json-rpc/2/invoke', {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+  })
+
+  const code = await response.json()
+  const randomCode = code.result.random.data[0]
+  return randomCode
+}
+var files=[]
+
+function clickInput(){
+  document.getElementById("input").click()
+}
+
+document.getElementById("input").addEventListener("change", function(){
+  files= this.files;
+});
+
+async function uploadImage(){
+    let downloadCode = await random();
+    document.querySelector("#code").innerHTML = downloadCode;
+    
+    const ref = await firebase.storage().ref();
+
+    for(let i=0;i<files.length;i++){
+      const name = files[i].name;
+      const metadata ={
+          contentType:files[i].type
+      }
+
+      if (files[i].size > 2097152) {
+        alert("File is too big!");
+        return;
+      }
+
+      const imageRef = await ref.child(downloadCode + "/" + name);
+      const snapshot = await imageRef.put(files[i],metadata);
+
+    }
+    alert("Upload Successful");
+
+    let data = {
+      "downloadCode":downloadCode,
+      "password":"",
+      "expireTime":""
+    }
+    // store in db
+    let res = await fetch(baseUrl+'/api/files', {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+  res = await res.json();
+  console.log(res);
+      
+}
